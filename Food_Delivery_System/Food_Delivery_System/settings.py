1907f9cdda7ac863eaa6365179735cab76e2640d
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-06-17 11:20:05 +0300

    Update settings.py

--- Food_Delivery_System/Food_Delivery_System/settings.py
+++ Food_Delivery_System/Food_Delivery_System/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 db_path = BASE_DIR / 'db.sqlite3'
-os.chmod(db_path, 0o640)
+os.chmod(db_path, 0o664)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
